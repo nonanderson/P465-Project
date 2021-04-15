61608c2f824dbeadf886d749478d202d600507de
asm9 <asm9@iu.edu>
2021-04-15 16:45:11 -0400

    fixed voice that i accidentally broke oops

--- public/script.js
+++ public/script.js
@@ -3,6 +3,10 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+const {username, room} = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+})
+
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
