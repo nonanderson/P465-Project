722f86a2f7f64d5597fc00370aaea0a9f7d3624a
Tannerhay1258 <54722528+Tannerhay1258@users.noreply.github.com>
2021-04-24 14:22:50 -0400

    changed a redirect

--- routes/users.js
+++ routes/users.js
@@ -138,7 +138,7 @@ router.post('/login',
 router.get('/logout', (req, res) => {
   req.logout();
   req.flash('success_msg', 'You are logged out');
-  res.redirect('login');
+  res.redirect('housing');
 });
 
 module.exports = router;
